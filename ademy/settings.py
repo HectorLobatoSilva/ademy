027c6ec612ecdd4440d9765afb0691078f5a3442
Hector Lobato <lobato_man@hotmail.com>
2020-10-19 10:13:58 -0500

    Update settings.py

--- ademy/settings.py
+++ ademy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hy25j5@v!d6c-5hmxvxh$hm5t!ex=4d8m_tn8b3d-zi4d(n+#7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
